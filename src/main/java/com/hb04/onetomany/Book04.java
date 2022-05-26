aeb261ee3f6d8582ea88d0f012b139dcc79a4ab1
Yunus Pektas <yunuspektas@protonmail.com>
2022-05-26 23:16:26 +0300

    05.26.2022

--- src/main/java/com/hb04/onetomany/Book04.java
+++ src/main/java/com/hb04/onetomany/Book04.java
@@ -1,5 +1,20 @@
 package com.hb04.onetomany;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Book04 {
+	
+	@Id  // otomatik import için bu ide de ctrl + shift+o ya basmak yeterli olur
+	private int id;
+	
+	private String name;
+	
+	@ManyToOne
+	private Student04 student;
+	
+	
 
 }
--- src/main/java/com/hb04/onetomany/Student04.java
+++ src/main/java/com/hb04/onetomany/Student04.java
@@ -1,20 +1,69 @@
 package com.hb04.onetomany;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Student04 {
-	
+
 	@Id
 	private int id;
-	//length : max karakter sayısını
-	//nullable  : ilgili kolonun mutlaka null olup olmayacagını belirler
-	//updatable : update edilebilir. mesela ilk create eden kişinin isminin değişmesini istemiyebiliriz
-	//insertable : 
+	// length : max karakter sayısını
+	// nullable : ilgili kolonun mutlaka null olup olmayacagını belirler
+	// updatable : update edilebilir. mesela ilk create eden kişinin isminin
+	// değişmesini istemiyebiliriz
+	// insertable : insert edilebilir
 	@Column(name = "std_name", length = 100, nullable = false, unique = true) // bunu yazsanda yazmasanda kolon
 																				// gerçekleşir
 	private String name;
+
+	private int grade;
+
+	@OneToMany
+	private List<Book04> bookList = new ArrayList<>();  // one to many ilişkisi için yazıldı. bir öğrencinin birden fazala kitabı olabilir.
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public List<Book04> getBookList() {
+		return bookList;
+	}
+
+	public void setBookList(List<Book04> bookList) {
+		this.bookList = bookList;
+	}
+
+	@Override
+	public String toString() {
+		return "Student04 [id=" + id + ", name=" + name + ", grade=" + grade + ", bookList=" + bookList + "]";
+	}
+	
+	
 
 }
